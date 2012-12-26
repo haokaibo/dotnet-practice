4494a9fbb2780e058b4e983ce3c958d3ef80fbf5
haokaibo@gmail.com <haokaibo@gmail.com@1ab47685-b382-e38c-0f5a-e6f4a8f8c282>
2012-12-26 09:11:52 +0000

    Added the source code for the DotNetPractice project.

---
+++ src/DotNetPractice/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DotNetPractice
+{
+    interface IA
+    {
+        void Method2();
+    }
+    interface IA1 : IA
+    {
+        void Method3();
+    }
+    abstract class A
+    {
+        public abstract void Method();
+    }
+    abstract class A1 : A { }
+    class A2 : A1, IA1
+    {
+        public override void Method()
+        {
+            Console.WriteLine(this);
+            Console.WriteLine(this.GetType().BaseType);
+        }
+
+
+        public void Method3()
+        {
+            Console.WriteLine("This is the method from IA1");
+        }
+
+        public void Method2()
+        {
+            Console.WriteLine("This is the method from IA");
+        }
+
+    }
+
+    class B
+    {
+        public virtual void Test() { Console.WriteLine("This is B"); }
+
+        public void Test1(int a, string b) { }
+        public void Test1(string b, int a) { }
+    }
+    class B1 : B
+    {
+        public override void Test() { Console.WriteLine("This is B1"); }
+    }
+    class Program
+    {
+        public static void TestByte()
+        {
+            Console.WriteLine("The min and max of byte are {0} and {1} respectively.", byte.MinValue, byte.MaxValue);
+        }
+        static void Main(string[] args)
+        {
+            IA1 a = new A2();
+            a.Method2();
+            a.Method3();
+            (a as A1).Method();
+
+            TestByte();
+
+
+            B1 b = new B1();
+            b.Test();
+            ((B)b).Test();
+
+            b.Test1(1, "a");
+
+            Console.Read();
+        }
+    }
+}
