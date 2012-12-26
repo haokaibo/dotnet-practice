22453d7e46bdbfd32f81b5629b907375a54c1205
haokaibo@gmail.com <haokaibo@gmail.com@1ab47685-b382-e38c-0f5a-e6f4a8f8c282>
2012-12-26 09:11:52 +0000

    Added the source code for the DotNetPractice project.

---
+++ trunk/src/DotNetPractice/OverrideGetHashcode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.CompilerServices;
+
+namespace DotNetPractice
+{
+    class OverrideGetHashcode
+    {
+        public override Int32 GetHashCode()
+        {
+            return RuntimeHelpers.GetHashCode(this);
+        }
+    }
+}
---
+++ trunk/src/DotNetPractice/TheSequenceOfFieldsInType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+namespace DotNetPractice
+{
+    [StructLayout(LayoutKind.Auto)]
+    class TheSequenceOfFieldsInTypeAuto : Object
+    {
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    class TheSequenceOfFieldsInTypeSequential
+    { }
+
+    [StructLayout(LayoutKind.Explicit)]
+    class TheSequenceOfFieldsExplicit
+    {
+        [FieldOffset(0)]
+        int test;
+    }
+}
