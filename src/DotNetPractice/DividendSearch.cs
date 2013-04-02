c58bc87767d9cc2378fc8c14aa4c9164eda9ec7c
haokaibo@gmail.com <haokaibo@gmail.com@1ab47685-b382-e38c-0f5a-e6f4a8f8c282>
2013-04-02 14:43:33 +0000

    Added the dividend search.

---
+++ src/DotNetPractice/DividendSearch.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DotNetPractice
+{
+    class SearchHelper
+    {
+        private int[] m_TargetArray;
+        private int m_TargetArrayLength;
+        private int m_SearchNum;
+        internal SearchHelper(int[] targetArray, int searchNum)
+        {
+            m_TargetArray = targetArray.Clone() as int[];
+            m_SearchNum = searchNum;
+            m_TargetArrayLength = m_TargetArray.Length;
+        }
+        /// <summary>
+        /// Search the array by dividend method
+        /// </summary>
+        /// <param name="lowerBound">The lower bound of the search range</param>
+        /// <param name="upperBound">The upper bound of the search range</param>
+        /// <returns>The target index of the position where the n should insert.</returns>
+        private int DividendSearch(int lowerBound, int upperBound)
+        {
+            if (m_TargetArrayLength == 0)
+            {
+                return 0;
+            }
+            if (lowerBound == upperBound - 1)
+            {
+                return lowerBound;
+            }
+            if (m_SearchNum > m_TargetArray[(lowerBound + upperBound) / 2])
+            {
+                return DividendSearch((lowerBound + upperBound) / 2, upperBound);
+            }
+            else
+            {
+                return DividendSearch(lowerBound, (lowerBound + upperBound) / 2);
+            }
+        }
+
+        public int DividendSearch()
+        {
+            return DividendSearch(0, m_TargetArrayLength);
+        }
+    }
+}
