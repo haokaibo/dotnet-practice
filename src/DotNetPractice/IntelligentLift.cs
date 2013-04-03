91cb95c55e4b795d391b35ae20afaf712d5842ca
haokaibo@gmail.com <haokaibo@gmail.com@1ab47685-b382-e38c-0f5a-e6f4a8f8c282>
2013-04-03 13:06:55 +0000

    Added the inteligentLift and Unit test project.

---
+++ src/DotNetPractice/IntelligentLift.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DotNetPractice
+{
+    public struct BestFloor
+    {
+        public BestFloor(int targetFloor, int minFloor)
+        {
+            nTargetFloor = targetFloor;
+            nMinFloor = minFloor;
+        }
+        public int nTargetFloor;
+        public int nMinFloor;
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (obj is BestFloor)
+            {
+                return this.Equals((BestFloor)(obj));
+            }
+            else
+                return false;
+        }
+        public bool Equals(BestFloor right)
+        {
+            if (this.nMinFloor == right.nMinFloor && this.nTargetFloor == right.nTargetFloor)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public class IntelligentLift
+    {
+        private int m_FloorCount = 0;
+        private int[] m_PersonTargets;
+
+        /// <summary>
+        /// The constructor of the IntelligentLift
+        /// </summary>
+        /// <param name="N">Floor count</param>
+        /// <param name="personTargets">The count of the person who go to the target floor</param>
+        public IntelligentLift(int N, int[] personTargets)
+        {
+            m_FloorCount = N;
+            m_PersonTargets = personTargets as int[];
+        }
+
+        public BestFloor GetTheBestFloor1()
+        {
+            BestFloor bf = new BestFloor() { nTargetFloor = -1 };
+
+            int personTargetCount = m_PersonTargets.Length;
+            for (int i = 1; i <= m_FloorCount; i++)
+            {
+                int nFloor = 0;
+                for (int j = 1; j < personTargetCount; j++)
+                {
+                    nFloor += Math.Abs((j - i) * m_PersonTargets[j]);
+                }
+                if (bf.nTargetFloor == -1 || nFloor < bf.nMinFloor)
+                {
+                    bf.nTargetFloor = i;
+                    bf.nMinFloor = nFloor;
+                }
+            }
+            return bf;
+        }
+
+        public BestFloor GetTheBestFloor2(){
+            return new BestFloor();
+        }
+    }
+}
---
+++ src/DotNetPracticeTest/IntelligentLiftTest.cs
@@ -0,0 +1,86 @@
+﻿using DotNetPractice;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DotNetPracticeTest
+{
+
+
+    /// <summary>
+    ///这是 IntelligentLiftTest 的测试类，旨在
+    ///包含所有 IntelligentLiftTest 单元测试
+    ///</summary>
+    [TestClass()]
+    public class IntelligentLiftTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///获取或设置测试上下文，上下文提供
+        ///有关当前测试运行及其功能的信息。
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region 附加测试特性
+        // 
+        //编写测试时，还可使用以下特性:
+        //
+        //使用 ClassInitialize 在运行类中的第一个测试前先运行代码
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //使用 ClassCleanup 在运行完类中的所有测试后再运行代码
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //使用 TestInitialize 在运行每个测试前先运行代码
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //使用 TestCleanup 在运行完每个测试后运行代码
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///GetTheBestFloor 的测试
+        ///</summary>
+        [TestMethod()]
+        public void GetTheBestFloor1Test()
+        {
+            int N = 6; // TODO: 初始化为适当的值
+            int[] personTargets = new int[N + 1]; // TODO: 初始化为适当的值
+            for (int i = 1; i <= N; i++)
+            {
+                personTargets[i] = i;
+            }
+            IntelligentLift target = new IntelligentLift(N, personTargets); // TODO: 初始化为适当的值
+            BestFloor expected = new BestFloor() { nTargetFloor = 2, nMinFloor = 4 }; // TODO: 初始化为适当的值
+            BestFloor actual;
+            actual = target.GetTheBestFloor1();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
