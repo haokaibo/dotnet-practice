1eeac1e4c1b9b0bf9adea9c2dc2ee2b355b6bcd2
haokaibo@gmail.com <haokaibo@gmail.com@1ab47685-b382-e38c-0f5a-e6f4a8f8c282>
2013-04-05 11:39:18 +0000

    Implemented the meeting helper.

---
+++ trunk/src/DotNetPractice/SortHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace DotNetPractice
+{
+    public class SortingHelper
+    {
+        private int[] m_TargetArray;
+        public SortingHelper(int[] targetArray)
+        {
+            if (null == targetArray || 0 == targetArray.Length)
+            {
+                throw new ArgumentNullException("'targetArray' should not be null and the length of it should not be 0.");
+            }
+            m_TargetArray = targetArray;
+        }
+
+        public void QuickSort(int targetNum, int targetIndex)
+        {
+            for (int i = m_TargetArray.Length - 1; i >= targetIndex; i--)
+            {
+                if (targetNum > m_TargetArray[i])
+                {
+                    m_TargetArray[targetIndex] = m_TargetArray[i];
+                    for (int j = targetIndex + 1; j < i; j++)
+                    {
+                        if (targetNum < m_TargetArray[j])
+                        {
+                            m_TargetArray[i] = m_TargetArray[j];
+                        }
+                    }
+                }
+            }
+        }
+
+        public void QuickSort()
+        {
+            int targetNum = m_TargetArray[0];
+            QuickSort(targetNum, 0);
+        }
+
+    }
+}
---
+++ trunk/src/DotNetPracticeTest/MeetingHelperTest.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using DotNetPractice;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DotNetPracticeTest
+{
+
+
+    /// <summary>
+    ///This is a test class for MeetingHelperTest and is intended
+    ///to contain all MeetingHelperTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class MeetingHelperTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for GetMinimumMeetRooms
+        ///</summary>
+        [TestMethod()]
+        public void GetMinimumMeetRoomsTest()
+        {
+            Meeting[] meetings = new Meeting[]{
+                new Meeting(new DateTime(2013,4,8,9,0,0), new DateTime(2013,4,8,10,0,0)),
+                new Meeting(new DateTime(2013,4,8,9,0,0), new DateTime(2013,4,8,9,30,0)),
+                new Meeting(new DateTime(2013,4,8,9,30,0), new DateTime(2013,4,8,10,0,0)),
+            };
+
+            // TODO: Initialize to an appropriate value
+            MeetingHelper target = new MeetingHelper(meetings); // TODO: Initialize to an appropriate value
+            int[] expected = new int[] { 1, 2, 2 }; // TODO: Initialize to an appropriate value
+            int[] actual;
+            actual = target.GetMinimumMeetRooms();
+
+            Assert.IsTrue(MeetingHelperTest.ArrayEquals(expected, actual));
+        }
+
+        public static bool ArrayEquals(int[] left, int[] right)
+        {
+            if (left == null || right == null)
+            {
+                return false;
+            }
+            if (left.Length != right.Length)
+            {
+                return false;
+            }
+            for (int i = 0; i < left.Length; i++)
+            {
+                if (left[i] == right[i])
+                {
+                    continue;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
