c437077d10a6e934d2b9bf843690119db7546f6d
haokaibo@gmail.com <haokaibo@gmail.com@1ab47685-b382-e38c-0f5a-e6f4a8f8c282>
2013-06-29 14:17:57 +0000

    1. Added the ForumHelper class and implemented the FindForumBestInvolver method and related test. 2. Added GetMinimumMeetRoomsTest, GetTheBestFloor1Test, GetTheBestFloor2Test, PointsGameTest and FindForumBestInvolver tests to the Beauty of programming test list.

---
+++ trunk/src/DotNetPractice/ForumHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DotNetPractice
+{
+    public class ForumHelper
+    {
+        private int[] m_IdList;
+        public ForumHelper(int[] idList)
+        {
+            m_IdList = idList;
+        }
+
+        public int FindForumBestInvolver()
+        {
+            int candidate = 0, nTimes = 0;
+            for (int i = 0, listLength = m_IdList.Length; i < listLength; i++)
+            {
+                if (nTimes == 0)
+                {
+                    candidate = m_IdList[i];
+                    nTimes = 1;
+                }
+                else
+                {
+                    if (candidate == m_IdList[i])
+                        nTimes++;
+                    else
+                        nTimes--;
+                }
+            }
+            return candidate;
+        }
+    }
+}
